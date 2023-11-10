3a2d07f109e142df3f65c350612cf392901eb703
Ariel Ribeiro <ariiel.ribeiro@gmail.com>
2023-11-09 20:37:09 -0500

    form components

---
+++ src/Components/Forms/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+interface Button {
+  children: string;
+}
+
+const Button = ({ children, ...props }: Button) => {
+  return (
+    <button {...props} className={styles.button}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
